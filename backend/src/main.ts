7841975f1efaad5fc556660a26b8296792746f7b
xuanduong1605 <xuanduong160505@gmail.com>
2025-04-16 08:58:50 +0700

    add backend setup to the app

---
+++ backend/src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', '..', 'frontend'));
+  app.setBaseViewsDir(join(__dirname, '..', '..', 'frontend'));
+
+  await app.listen(3003);
+}
+bootstrap();
